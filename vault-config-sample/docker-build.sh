a7607247fa7277d96559651e5122c04fddd3a9a1
Thiago Locatelli <thiago.locatelli@gmail.com>
2019-05-13 04:01:30 -0400

    trying to create native image

--- vault-config-sample/docker-build.sh
+++ vault-config-sample/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build . -t complete
+docker build . -t vault-config-sample -m 2g
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 complete"
+echo "    $ docker run -p 8080:8080 vault-config-sample"
